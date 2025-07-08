5b7ecafce04abd44001cb739b78471a252c1efde
Maxim Grigorev <maxkraft06@gmail.com>
2025-07-08 11:52:52 +0300

    huita

--- g.c
+++ g.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+int summary(int a, int b) {
+    return a + b;
+}
+
 int main() {
+    int x = 5;
+    int y = 10;
+    int result = summary(x, y);
+    printf("The sum of %d and %d is %d\n", x, y, result);
     return 0;
 }
