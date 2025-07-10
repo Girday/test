1880bde2b2b8206e3687895c3496d5b26e6792cb
Maxim Grigorev <maxkraft06@gmail.com>
2025-07-10 15:37:20 +0300

    -evrth

--- bbu.rb
+++
@@ -1,4 +0,0 @@
-puts "Привет, мир!"
-numbers = [1, 2, 3, 4, 5]
-squares = numbers.map { |n| n * n }
-puts "Квадраты: #{squares}"
--- bir.R
+++
@@ -1,4 +0,0 @@
-print("Привет, мир!")
-numbers <- c(1, 2, 3, 4, 5)
-squares <- numbers^2
-print(paste("Квадраты:", paste(squares, collapse=", ")))
