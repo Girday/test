0052efbd1760fd09d5563a60d96f3abf046c6758
Max <123688667+Girday@users.noreply.github.com>
2025-07-11 11:03:50 +0300

    Delete asda.fs

--- asda.fs
+++
@@ -1,7 +0,0 @@
-[<EntryPoint>]
-let main argv =
-    printfn "Привет, мир!"
-    let numbers = [1; 2; 3; 4; 5]
-    let squares = List.map (fun x -> x * x) numbers
-    printfn "Квадраты: %A" squares
-    0
