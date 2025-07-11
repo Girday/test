7178a5dbcf51111f003588c15f7daa1c8737e71c
Maxim Grigorev <maxkraft06@gmail.com>
2025-07-11 17:04:17 +0300

    fi

--- main.py
+++ main.py
@@ -1,5 +1,11 @@
-c = 0
-for i in range(100, 201):
-    if i % 2 == 0:
-        c += 1
-print(c)
+starosta = 750
+standard = 4500
+
+pov1, pov2 = 7490, 14980
+
+c = 1
+for var in [0, pov1, pov2]:
+    x = sum([standard, starosta, var]) * 0.97
+    print(f"Var{c}: {x}, Sem: {x * 5}, Total: {x * 5 + 127599}")
+    c += 1
+
